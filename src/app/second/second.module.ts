d423c008b162fe327397308528a296b01a73d870
Yasser <yasser.elmalouani@ulixe.com>
2019-02-25 17:40:00 +0100

    first commit

---
+++ src/app/second/second.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SecondComponent } from './second.component';
+import { SecondRoutingModule } from './second-routing.module';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    SecondRoutingModule
+  ],
+  declarations: [SecondComponent]
+})
+export class SecondModule { }

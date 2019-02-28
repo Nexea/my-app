fc111e2b22cbbdb6310227d3ecd32b5efff841cd
Yasser <yasser.elmalouani@ulixe.com>
2019-02-28 17:22:09 +0100

    aggiunti effetti scroll

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,17 +11,6 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    // let skillCard = document.getElementById('tec_skills')
-    // let bounding = skillCard.getBoundingClientRect();
-
-    // if (bounding.top >= 0 && bounding.left >= 0 && bounding.right <= (window.innerWidth || document.documentElement.clientWidth) &&
-    //   bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)
-    // ) {
-    //   skillCard.classList.add('rollIn')
-    //   console.log('In the viewport!');
-    // } else {
-    //   console.log('Not in the viewport');
-    // }
   }
 
 }
